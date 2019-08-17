cff4177593fadd3d21cd28a3b6eb4987d89581af
jonasxd360 <37018342+jonasxd360@users.noreply.github.com>
2019-08-17 19:27:56 +0200

    strange blue loading circle

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -2,21 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+import 'backdrop.dart';
 import 'category.dart';
 import 'category_tile.dart';
 import 'unit.dart';
-
-import 'backdrop.dart';
 import 'unit_converter.dart';
 
-final _backgroundColor = Colors.green[100];
-
-/// Category Route (screen).
+/// Loads in unit conversion data, and displays the data.
 ///
-/// This is the 'home' screen of the Unit Converter. It shows a header and
-/// a list of [Categories].
+/// This is the main screen to our app. It retrieves conversion data from a
+/// JSON asset and from an API. It displays the [Categories] in the back panel
+/// of a [Backdrop] widget and shows the [UnitConverter] in the front panel.
 ///
 /// While it is named CategoryRoute, a more apt name would be CategoryScreen,
 /// because it is responsible for the UI at the route's destination.
@@ -28,11 +29,14 @@ class CategoryRoute extends StatefulWidget {
 }
 
 class _CategoryRouteState extends State<CategoryRoute> {
-  // TODO: Keep track of a default [Category], and the currently-selected
-  var _category;
-  var _defaultCategory;
-  // [Category]
+  Category _defaultCategory;
+  Category _currentCategory;
+  // Widgets are supposed to be deeply immutable objects. We can update and edit
+  // _categories as we build our app, and when we pass it into a widget's
+  // `children` property, we call .toList() on it.
+  // For more details, see https://github.com/dart-lang/sdk/issues/27755
   final _categories = <Category>[];
+  // TODO: Remove _categoryNames as they will be retrieved from the JSON asset
   static const _categoryNames = <String>[
     'Length',
     'Area',
@@ -79,33 +83,62 @@ class _CategoryRouteState extends State<CategoryRoute> {
     }),
   ];
 
-  @override
-  void initState() {
-    super.initState();
-    // TODO: Set the default [Category] for the unit converter that opens
-    for (var i = 0; i < _categoryNames.length; i++) {
-      _categories.add(Category(
-        name: _categoryNames[i],
-        color: _baseColors[i],
-        iconLocation: Icons.cake,
-        units: _retrieveUnitList(_categoryNames[i]),
-      ));
+  //  Remove the overriding of initState(). Instead, we use
+  // didChangeDependencies()
+//  @override
+//  void initState() {
+//    super.initState();
+//    for (var i = 0; i < _categoryNames.length; i++) {
+//      var category = Category(
+//        name: _categoryNames[i],
+//        color: _baseColors[i],
+//        iconLocation: Icons.cake,
+//        units: _retrieveUnitList(_categoryNames[i]),
+//      );
+//      if (i == 0) {
+//        _defaultCategory = category;
+//      }
+//      _categories.add(category);
+//    }
+//  }
+
+  // Uncomment this out. We use didChangeDependencies() so that we can
+  // wait for our JSON asset to be loaded in (async).
+    @override
+    Future<void> didChangeDependencies() async {
+      super.didChangeDependencies();
+      // We have static unit conversions located in our
+      // assets/data/regular_units.json
+      if (_categories.isEmpty) {
+        await _retrieveLocalCategories();
+      }
     }
-    _category = _categories[0];
-    _defaultCategory = _categories[0];
+
+  /// Retrieves a list of [Categories] and their [Unit]s
+  Future<void> _retrieveLocalCategories() async {
+    // Consider omitting the types for local variables. For more details on Effective
+    // Dart Usage, see https://www.dartlang.org/guides/language/effective-dart/usage
+    final json = DefaultAssetBundle
+        .of(context)
+        .loadString('assets/data/regular_units.json');
+    final data = JsonDecoder().convert(await json);
+    if (data is! Map) {
+      throw ('Data retrieved from API is not a Map');
+    }
+    // TODO: Create Categories and their list of Units, from the JSON asset
   }
 
-  // TODO: Fill out this function
   /// Function to call when a [Category] is tapped.
   void _onCategoryTap(Category category) {
     setState(() {
-      _category = category;
+      _currentCategory = category;
     });
   }
 
-  /// Makes the correct number of rows for the list view.
+  /// Makes the correct number of rows for the list view, based on whether the
+  /// device is portrait or landscape.
   ///
-  /// For portrait, we use a [ListView].
+  /// For portrait, we use a [ListView]. For landscape, we use a [GridView].
   Widget _buildCategoryWidgets(Orientation deviceOrientation) {
     if (deviceOrientation == Orientation.portrait) {
       return ListView.builder(
@@ -131,8 +164,11 @@ class _CategoryRouteState extends State<CategoryRoute> {
     }
   }
 
+  // TODO: Delete this function; instead, read in the units from the JSON asset
+  // inside _retrieveLocalCategories()
   /// Returns a list of mock [Unit]s.
   List<Unit> _retrieveUnitList(String categoryName) {
+    // when the app first starts up
     return List.generate(10, (int i) {
       i += 1;
       return Unit(
@@ -144,6 +180,16 @@ class _CategoryRouteState extends State<CategoryRoute> {
 
   @override
   Widget build(BuildContext context) {
+    if (_categories.isEmpty) {
+      return Center(
+        child: Container(
+          height: 180.0,
+          width: 180.0,
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     // Based on the device size, figure out how to best lay out the list
     // You can also use MediaQuery.of(context).size to calculate the orientation
     assert(debugCheckHasMediaQuery(context));
@@ -155,13 +201,12 @@ class _CategoryRouteState extends State<CategoryRoute> {
       ),
       child: _buildCategoryWidgets(MediaQuery.of(context).orientation),
     );
-
     return Backdrop(
       currentCategory:
-      _category == null ? _defaultCategory : _category,
-      frontPanel: _category == null
+      _currentCategory == null ? _defaultCategory : _currentCategory,
+      frontPanel: _currentCategory == null
           ? UnitConverter(category: _defaultCategory)
-          : UnitConverter(category: _category),
+          : UnitConverter(category: _currentCategory),
       backPanel: listView,
       frontTitle: Text('Unit Converter'),
       backTitle: Text('Select a Category'),
